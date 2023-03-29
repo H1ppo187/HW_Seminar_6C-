b35648a126d9526fedefd9a299eb1079f27b8d62
Alexander Balashov <balashov187@yandex.ru>
2023-03-29 22:00:04 +0300

    задача 41

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите числа через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Console.WriteLine(SumOfEven(array));
+
+// ----------Метод---------------
+int SumOfEven(int[] newArray)
+{   
+    int count =0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
